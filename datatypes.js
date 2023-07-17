224cb61c04232f5b084631c47803a3b160b6548e
Yuvadi29 <adityatrivedi612@gmail.com>
2023-07-17 14:52:19 +0530

    DataTypes in JavaScript - #SHORTS2 ✅

---
+++ datatypes.js
@@ -0,0 +1,33 @@
+// Numbers:
+let length = 16;
+let weight = 7.5;
+console.log(typeof(weight));
+
+// Strings:
+let color = "Yellow";
+console.log(typeof(color));
+
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {
+    firstName:"John", 
+    lastName:"Doe"
+};
+
+// Array object:
+const cars = [
+    "BMW", 
+    "Volvo"
+];
+
+// Date object:
+const date = new Date(
+    "2022-03-25"
+);
+console.log(typeof(date));
+
