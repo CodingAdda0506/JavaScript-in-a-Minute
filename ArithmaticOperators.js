6504afb37f538c0219f332106541c677f39b303d
Yuvadi29 <adityatrivedi612@gmail.com>
2023-07-30 15:23:26 +0530

    Arithmetic Operators Completed

---
+++ ArithmaticOperators.js
@@ -0,0 +1,29 @@
+/*
+Arithmetic Operators
+Addition: +
+Subtraction: -
+Multiplication: *
+Division: /
+Modulus: %
+Exponentiation: **
+*/
+
+let a = 100;
+let b = 5;
+// Additoin :- 
+console.log(a + b);
+
+// Subtraction
+console.log(a - b);
+
+// Multiplication
+console.log(a * b);
+
+// Division
+console.log(a / b);
+
+// Modulus
+console.log(a % b);
+
+// Exponent
+console.log(a ** b);
