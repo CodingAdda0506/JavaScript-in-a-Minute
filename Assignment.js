071b9bf6fb93c9915e37cb9ce0e8e00ada6f12bc
Yuvadi29 <adityatrivedi612@gmail.com>
2023-07-30 15:23:40 +0530

    Assignment Operators Completed

---
+++ Assignment.js
@@ -0,0 +1,44 @@
+/*
+Assignment Operators:
+
+Assignment: =
+Addition assignment: +=
+Subtraction assignment: -=
+Multiplication assignment: *=
+Division assignment: /=
+Remainder assignment (Modulus): %=
+Exponentiation assignment: **=
+*/
+
+let a = 100;
+let b = 5;
+
+// Assignment
+let assignment = a;
+console.log(assignment);
+
+// Addition assignment
+assignment += b;
+
+assignment = assignment + b;
+console.log(assignment);
+
+// Subtraction assignment
+assignment -= b;
+console.log(assignment);
+
+// Multiplication assignment
+assignment *= b;
+console.log(assignment);
+
+// Division assignment
+assignment /= b;
+console.log(assignment);
+
+// Remainder assignment
+assignment %= b;
+console.log(assignment);
+
+// Exponentiation assignment
+assignment **= b;
+console.log(assignment);
