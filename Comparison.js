0d7b182328b85dae029488a1fbed5980d47112df
Yuvadi29 <adityatrivedi612@gmail.com>
2023-08-06 12:30:15 +0530

    Comparison Operators Completed

---
+++ Comparison.js
@@ -0,0 +1,27 @@
+// Comparison Operators
+let num1 = 10;
+let num2 = 5;
+
+// Equality (loose equality, type coercion)
+console.log(num1 == num2); // false
+
+// Strict Equality (value and type equality)
+console.log(num1 === num2); // false
+
+// Inequality (loose inequality, type coercion)
+console.log(num1 != num2); // true
+
+// Strict Inequality (value or type inequality)
+console.log(num1 !== num2); // true 
+
+// Greater than
+console.log(num1 > num2); // true
+
+// Less than
+console.log(num1 < num2); // false
+
+// Greater than or equal to
+console.log(num1 >= num2); // true
+
+// Less than or equal to
+console.log(num1 <= num2); // false
