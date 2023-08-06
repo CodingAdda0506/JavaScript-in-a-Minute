c9db07022bc365b818bfc8e5144fc9be217c0eb6
Yuvadi29 <adityatrivedi612@gmail.com>
2023-08-06 12:41:29 +0530

    Logical Operators Completed

---
+++ Logical.js
@@ -0,0 +1,16 @@
+var adult = true;
+var gender = "Male";
+
+// Logical AND 
+if(gender == "Male" && adult == true)
+    console.log("Adult Male");
+
+// Logical OR 
+if(gender == "Male" || adult == true)
+    console.log("Adult Male");
+
+// Logical NOT 
+if(!adult)
+    console.log("Adult");
+else
+    console.log("Not Adult");
