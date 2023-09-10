18e469d816edf3bece9205326b58715fe2a6c26a
Yuvadi29 <adityatrivedi612@gmail.com>
2023-09-10 17:05:46 +0530

    For Loop Completed

---
+++ ForLoop.js
@@ -0,0 +1,11 @@
+// For Loop
+
+// for (let i = initial_value; i < final_value; i++) {
+//     // Code to be executed in each iteration
+// }
+
+// Print a string 10 times 
+
+for(let i = 1; i <= 10; i++) {
+    console.log("Coding Adda");
+}
