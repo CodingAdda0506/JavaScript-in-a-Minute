5e60c3398c5bccbcf7b3978082e29d6afd274a0d
Yuvadi29 <adityatrivedi612@gmail.com>
2023-09-10 16:46:17 +0530

    If-Else Completed

---
+++ IfandElse.js
@@ -0,0 +1,15 @@
+// if-else StateMent
+
+// if (condition) {
+//     // Code to execute if the condition is true
+// } else {
+//     // Code to execute if the condition is false
+// }
+
+let age = 18;
+
+if(age >= 18) {
+    console.log("Adult");
+} else {
+    console.log("Not Adult");
+}
