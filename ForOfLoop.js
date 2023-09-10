bf98e1009d67958d1317b089f9e818f62301de43
Yuvadi29 <adityatrivedi612@gmail.com>
2023-09-10 17:06:04 +0530

    For of Loop Completed

---
+++ ForOfLoop.js
@@ -0,0 +1,18 @@
+// for-of loop
+
+// for (const element of iterable) {
+//        Code to be executed for 
+//        each element in the 
+//        iterable
+//   }
+
+const fruits = [
+    'Apple', 
+    'Banana', 
+    'Cherry'
+];
+
+
+for(const fruit of fruits) {
+    console.log(fruit);
+}
