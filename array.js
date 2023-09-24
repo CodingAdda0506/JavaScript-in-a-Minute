0d83e0afcd0ab8cd206fa1593f80eea4517d7171
Yuvadi29 <adityatrivedi612@gmail.com>
2023-09-24 16:49:52 +0530

    Array Completed

---
+++ array.js
@@ -0,0 +1,15 @@
+/*
+    Array in Javascript
+*/
+
+const carName =[
+    'Supra',
+    'Skyline r34',
+    'GTR r33',
+    'Silvia s15',
+    'Subaru'
+];
+
+carName.forEach(car => {
+    console.log(car);
+})
