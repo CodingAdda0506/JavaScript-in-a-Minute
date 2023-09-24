36c80e9ae66c0156118f795e9d9b67b3c09dc057
Yuvadi29 <adityatrivedi612@gmail.com>
2023-09-24 16:48:10 +0530

    FUNCITONS COMPLETED

---
+++ function.js
@@ -0,0 +1,17 @@
+/*
+    FUNCTION IN JAVASCRIPT
+
+    function function_name() {
+        // code here
+    }
+
+*/
+
+let name = "Coding Adda";
+let age = 2.5;
+
+function print(name, age) {
+    console.log(`Our channel's name is ${name} and our channel's age is ${age} years.`);
+}
+
+print(name, age);
